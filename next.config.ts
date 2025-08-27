27ba06e3bf11863c749f23f078b983ff6851a513
KhanAnupamShafi <deep71.bd@gmail.com>
2025-08-27 19:38:58 +0600

    configs added

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
     unoptimized: true,
   },
   output: "export",
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/dasher-ui" : "",
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, "node_modules")],
   },
