065479b47fd9de8c54d68f0e511bce0fd4cbca98
KhanAnupamShafi <deep71.bd@gmail.com>
2025-08-28 10:16:48 +0600

    footer added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,14 +7,19 @@ import Flex from "components/common/Flex";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Flex
-      tag="main"
-      direction="column"
-      justifyContent="center"
-      className="vh-100"
-    >
+      tag='main'
+      direction='column'
+      justifyContent='center'
+      className='vh-100'>
       <section>
         <Container>{children}</Container>
       </section>
+      <div className='custom-container'>
+        <span className='me-1'>Theme distributed by - </span>
+        <a href='https://www.themewagon.com/' target='_blank' rel='noopener '>
+          ThemeWagon
+        </a>
+      </div>
     </Flex>
   );
 };
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,10 +9,16 @@ interface DashboardProps {
 const DashboardLayout: React.FC<DashboardProps> = ({ children }) => {
   return (
     <div>
-      <Sidebar hideLogo={false} containerId="miniSidebar" />
-      <div id="content" className="position-relative h-100">
+      <Sidebar hideLogo={false} containerId='miniSidebar' />
+      <div id='content' className='position-relative h-100'>
         <Header />
-        <div className="custom-container">{children}</div>
+        <div className='custom-container'>{children}</div>
+        <div className='custom-container'>
+          <span className='me-1'>Theme distributed by - </span>
+          <a href='https://www.themewagon.com/' target='_blank' rel='noopener '>
+            ThemeWagon
+          </a>
+        </div>
       </div>
     </div>
   );
